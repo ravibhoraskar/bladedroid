c8812ef1b4253b290daa96b80fc91207dde68274
Ravi Bhoraskar <ravi@metapod>
2014-01-30 15:08:33 -0800

    Simple app added. Blade works

---
+++ workspace/SimpleApp/src/edu/uw/simpleapp/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.uw.simpleapp;
+
+import edu.uw.bladedroid.BladeDroid;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		BladeDroid.onCreate(this, savedInstanceState);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
