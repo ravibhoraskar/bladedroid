515b08fedcea622b11f6e45f3cbb21b66134329f
Dominic Langenegger <dola@gmx.ch>
2014-02-01 21:15:50 -0800

    updated bladedroid DexLoader to work in a more generic way (see docs)

--- workspace/DexLoader/src/edu/uw/bladedroid/BladeDroid.java
+++ workspace/DexLoader/src/edu/uw/bladedroid/BladeDroid.java
@@ -1,5 +1,7 @@
 package edu.uw.bladedroid;
 
+import java.util.Set;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -7,85 +9,97 @@ import android.util.Log;
 import edu.uw.bladedroid.loader.BladeLoader;
 
 public final class BladeDroid {
-	public static final String TAG = "BLADE-DROID";
-	
-	private static BladeDroid instance;
-	private BladeLoader loader;
-	
-	private BladeDroid(Activity initiator) {
-		loader = new BladeLoader(initiator);
-	}
-	
-	public static synchronized BladeDroid getInstance(Activity initiator) {
-		if(instance == null) {
-			instance = new BladeDroid(initiator);
-		}
-		return instance;
-	}
-	
-	public static void onCreate(Activity a, Bundle savedInstanceState) {
-		AbstractBlade b = retrieveBlade(a);
-		if (b != null) {
-			Log.i(TAG, "found blade, executing onCreate");
-			b.onCreate(a, savedInstanceState);
-		}
-	}
-	
-	public static void onStart(Activity a, Bundle savedInstanceState) {
-		AbstractBlade b = retrieveBlade(a);
-		if (b != null) {
-			Log.i(TAG, "found blade, executing onStart");
-			b.onStart(a, savedInstanceState);
-		}
-	}
-	
-	public static void onResume(Activity a, Bundle savedInstanceState) {
-		AbstractBlade b = retrieveBlade(a);
-		if (b != null) {
-			Log.i(TAG, "found blade, executing onResume");
-			b.onResume(a, savedInstanceState);
-		}
-	}
-	
-	public static void onPause(Activity a, Bundle savedInstanceState) {
-		AbstractBlade b = retrieveBlade(a);
-		if (b != null) {
-			Log.i(TAG, "found blade, executing onPause");
-			b.onPause(a, savedInstanceState);
-		}
-	}
-	
-	public static void onStop(Activity a, Bundle savedInstanceState) {
-		AbstractBlade b = retrieveBlade(a);
-		if (b != null) {
-			Log.i(TAG, "found blade, executing onStop");
-			b.onStop(a, savedInstanceState);
-		}
-	}
-	
-	public static void onDestroy(Activity a, Bundle savedInstanceState) {
-		AbstractBlade b = retrieveBlade(a);
-		if (b != null) {
-			Log.i(TAG, "found blade, executing onDestroy");
-			b.onDestroy(a, savedInstanceState);
-		}
-	}
-	
-	private static AbstractBlade retrieveBlade(Activity a) {
-		// change identifier to something different than appName
-		String appName = getApplicationName(a.getApplicationContext());
-		Log.i(TAG, "calling onCreate for app " + appName + " in activity " + a.getPackageName());
-		AbstractBlade b = getInstance(a).getBladeLoader().getBlade(appName);
-		return b;
-	}
-	
-	private BladeLoader getBladeLoader() {
-		return loader;
-	}
-	
-	private static String getApplicationName(Context context) {
-	    int stringId = context.getApplicationInfo().labelRes;
-	    return context.getString(stringId);
-	}
-	
+    public static final String TAG = "BLADE-DROID";
+
+    private static BladeDroid instance;
+    private final BladeLoader loader;
+
+    private BladeDroid(Activity initiator) {
+        loader = new BladeLoader(initiator);
+    }
+
+    public static synchronized BladeDroid getInstance(Activity initiator) {
+        if (instance == null) {
+            instance = new BladeDroid(initiator);
+        }
+        return instance;
+    }
+
+    public static void onCreate(Activity activity, Bundle savedInstanceState) {
+        executeBlades(activity, savedInstanceState, new BladeExecutor() {
+            @Override
+            public void execute(Activity activity, Bundle savedInstanceState, AbstractBlade b) {
+                b.onCreate(activity, savedInstanceState);
+            }
+        });
+    }
+
+    public static void onStart(Activity activity, Bundle savedInstanceState) {
+        executeBlades(activity, savedInstanceState, new BladeExecutor() {
+            @Override
+            public void execute(Activity activity, Bundle savedInstanceState, AbstractBlade b) {
+                b.onStart(activity, savedInstanceState);
+            }
+        });
+    }
+
+    public static void onResume(Activity activity, Bundle savedInstanceState) {
+        executeBlades(activity, savedInstanceState, new BladeExecutor() {
+            @Override
+            public void execute(Activity activity, Bundle savedInstanceState, AbstractBlade b) {
+                b.onResume(activity, savedInstanceState);
+            }
+        });
+    }
+
+    public static void onPause(Activity activity, Bundle savedInstanceState) {
+        executeBlades(activity, savedInstanceState, new BladeExecutor() {
+            @Override
+            public void execute(Activity activity, Bundle savedInstanceState, AbstractBlade b) {
+                b.onPause(activity, savedInstanceState);
+            }
+        });
+    }
+
+    public static void onStop(Activity activity, Bundle savedInstanceState) {
+        executeBlades(activity, savedInstanceState, new BladeExecutor() {
+            @Override
+            public void execute(Activity activity, Bundle savedInstanceState, AbstractBlade b) {
+                b.onStop(activity, savedInstanceState);
+            }
+        });
+    }
+
+    public static void onDestroy(Activity activity, Bundle savedInstanceState) {
+        executeBlades(activity, savedInstanceState, new BladeExecutor() {
+            @Override
+            public void execute(Activity activity, Bundle savedInstanceState, AbstractBlade b) {
+                b.onDestroy(activity, savedInstanceState);
+            }
+        });
+    }
+
+    private static void executeBlades(Activity activity, Bundle savedInstanceState, BladeExecutor executor) {
+        Set<AbstractBlade> blds = getInstance(activity).getBladeLoader().getBlades();
+        for (AbstractBlade b : blds) {
+            if (b != null) {
+                Log.i(TAG, "executing blade " + b);
+                executor.execute(activity, savedInstanceState, b);
+            }
+        }
+    }
+
+    private BladeLoader getBladeLoader() {
+        return loader;
+    }
+
+    private static String getApplicationName(Context context) {
+        int stringId = context.getApplicationInfo().labelRes;
+        return context.getString(stringId);
+    }
+
+    private interface BladeExecutor {
+        void execute(Activity a, Bundle savedIntanceState, AbstractBlade b);
+    }
+
 }
--- workspace/DexLoader/src/edu/uw/bladedroid/loader/BladeLoader.java
+++ workspace/DexLoader/src/edu/uw/bladedroid/loader/BladeLoader.java
@@ -2,108 +2,125 @@ package edu.uw.bladedroid.loader;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.util.HashMap;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Locale;
-import java.util.Map;
-
-import dalvik.system.DexClassLoader;
-
-import edu.uw.bladedroid.BladeDroid;
-import edu.uw.bladedroid.AbstractBlade;
+import java.util.Set;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Environment;
 import android.util.Log;
+import dalvik.system.DexClassLoader;
+import dalvik.system.DexFile;
+import edu.uw.bladedroid.AbstractBlade;
+import edu.uw.bladedroid.BladeDroid;
 
 /**
  * Loads all Blades from a specified location into memory for later use.
  */
 public class BladeLoader {
-	
-	// note that this needs the READ_EXTERNAL_STORAGE permission
-	// http://developer.android.com/reference/android/Manifest.permission.html#READ_EXTERNAL_STORAGE
-	private static final String FOLDER_NAME = "blades";
-	private static final File BASE_PATH = new File(Environment.getExternalStorageDirectory(), FOLDER_NAME);
-	/**
-	 * directory where optimized dex files should be written; must not be null
-	 */
-	private final File tmpDir;
-	
-	private Context applicationContext;
-	private Map<String, AbstractBlade> blades;
-	
-	public BladeLoader(Activity initiator) {
-		applicationContext = initiator.getApplicationContext();
-		tmpDir = getContext().getDir("dex", Context.MODE_PRIVATE);
-		blades = new HashMap<String, AbstractBlade>();
-	
-		loadAllBlades();
-	}
-	
-	
-	private void loadAllBlades() {
-		FileFilter filter = new FileFilter() {
-			@Override
-			public boolean accept(File pathname) {
-				return pathname.isFile() && isJar(pathname.getName());
-			}
-		};
-		
-//		String jarFiles = TextUtils.join(File.pathSeparator, );
 
-		if(!BASE_PATH.exists() || !BASE_PATH.isDirectory()) {
-			Log.i(BladeDroid.TAG, "No blades found because directory " +BASE_PATH+ " does not exit");
-			return;
-		}
-		
-		Log.i(BladeDroid.TAG, "Analyzing folder " + BASE_PATH);
-		File[] jarFiles = BASE_PATH.listFiles(filter);
-		
-		if(jarFiles == null) {
-			Log.e(BladeDroid.TAG, "Faied to load jar files from folder. Probably READ_EXTERNAL_STORAGE permission missing.");
-			return;
-		}
-		
-		for (File f : jarFiles) {
-			Log.i(BladeDroid.TAG, "Loading file " + f.getAbsolutePath());
-			// http://stackoverflow.com/questions/3022454/how-to-load-a-java-class-dynamically-on-android-dalvik
-			// use separate classLoader for every jar file (classes could have same names etc) 
-			DexClassLoader classLoader = new DexClassLoader(f.getPath(), 
-					tmpDir.getPath(), null, 
-					getClass().getClassLoader());
-			
-			// TODO: improve this to load all that implement IBlade etc.
-			// this is just temporary for testing
-			String className = "edu.uw.bladedroid.blade.TestBlade";
-			try {
-				Class<?> cls = Class.forName(className, true, classLoader);
-				Log.i(BladeDroid.TAG, "loaded class " + className + " sucessfully for " +getPlainFilename(f.getName())+ "!");
-				blades.put(getPlainFilename(f.getName()), (AbstractBlade) cls.newInstance());
-			} catch (Throwable t) {
-	            Log.e(BladeDroid.TAG, "Error while loading class from jar file " + f.getPath(), t);
-	        }
-		}
-		
-	}
-	
-	public AbstractBlade getBlade(String appName) {
-		return blades.get(appName);
-	}
-	
-	private String getPlainFilename(String filename) {
-		return filename.substring(0,  filename.length() - 4);
-	}
-	
-	private Context getContext() {
-		return applicationContext;
-	}
-	
-	private Locale getLocale() {
-		return getContext().getResources().getConfiguration().locale;
-	}
-	
-	private boolean isJar(String filename) {
-		return filename.toLowerCase(getLocale()).endsWith(".jar");
-	}
+    // note that this needs the READ_EXTERNAL_STORAGE permission
+    // http://developer.android.com/reference/android/Manifest.permission.html#READ_EXTERNAL_STORAGE
+    private static final String FOLDER_NAME = "blades";
+    private static final File BASE_PATH = new File(Environment.getExternalStorageDirectory(), FOLDER_NAME);
+    /**
+     * directory where optimized dex files should be written; must not be null
+     */
+    private final File tmpDir;
+    private final File bladeDir;
+
+    private final Context applicationContext;
+    private final Set<AbstractBlade> blades;
+
+    public BladeLoader(Activity initiator) {
+        applicationContext = initiator.getApplicationContext();
+        tmpDir = getContext().getDir("dex", Context.MODE_PRIVATE);
+        blades = new HashSet<AbstractBlade>();
+
+        // Only .jar files in the bladeDir are loaded
+        // .jar files in subfolder with package name of application
+        bladeDir = new File(BASE_PATH + File.separator + getApplicationPackageName());
+        bladeDir.mkdirs();
+
+        loadAllBlades();
+    }
+
+    private void loadAllBlades() {
+        FileFilter filter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isFile() && isJar(pathname.getName());
+            }
+        };
+
+        Log.i(BladeDroid.TAG, "Analyzing folder " + bladeDir);
+        File[] jarFiles = bladeDir.listFiles(filter);
+
+        if (jarFiles == null) {
+            Log.e(BladeDroid.TAG, "Faied to load jar files from folder. Probably READ_EXTERNAL_STORAGE permission missing.");
+            return;
+        }
+
+        for (File f : jarFiles) {
+            Log.i(BladeDroid.TAG, "Loading file " + f.getAbsolutePath());
+            loadBlades(f);
+        }
+
+    }
+
+    public void loadBlades(File jarFile) {
+        DexClassLoader classLoader = new DexClassLoader(jarFile.getPath(),
+                tmpDir.getPath(), null, getClass().getClassLoader());
+
+        try {
+            DexFile df = DexFile.loadDex(jarFile.getAbsolutePath(), new File(tmpDir, jarFile.getName() + ".odex").getAbsolutePath(), 0);
+            for (Enumeration<String> iter = df.entries(); iter.hasMoreElements();) {
+                String className = iter.nextElement();
+                System.out.println("Found class: " + className);
+                Class<?> cls = Class.forName(className, true, classLoader);
+                Log.i(BladeDroid.TAG, "loaded class " + className + " sucessfully for " + getPlainFilename(jarFile.getName()) + "!");
+                if (AbstractBlade.class.isAssignableFrom(cls)) {
+                    // blade class found
+                    Log.i(BladeDroid.TAG, "Found Blade class: " + className);
+                    blades.add((AbstractBlade) cls.newInstance());
+                }
+            }
+        } catch (IOException e) {
+            Log.e(BladeDroid.TAG, "Error while receiving classes from " + jarFile, e);
+        } catch (ClassNotFoundException e) {
+            Log.e(BladeDroid.TAG, "Error while loading class from " + jarFile, e);
+        } catch (InstantiationException e) {
+            Log.e(BladeDroid.TAG, "Could not create instance of blade in " + jarFile, e);
+        } catch (IllegalAccessException e) {
+            Log.e(BladeDroid.TAG, "Error when accessing class", e);
+        }
+
+    }
+
+    public Set<AbstractBlade> getBlades() {
+        return blades;
+    }
+
+    private String getPlainFilename(String filename) {
+        return filename.substring(0, filename.length() - 4);
+    }
+
+    private String getApplicationPackageName() {
+        return getContext().getApplicationInfo().packageName;
+    }
+
+    private Context getContext() {
+        return applicationContext;
+    }
+
+    private Locale getLocale() {
+        return getContext().getResources().getConfiguration().locale;
+    }
+
+    private boolean isJar(String filename) {
+        return filename.toLowerCase(getLocale()).endsWith(".jar");
+    }
 }
