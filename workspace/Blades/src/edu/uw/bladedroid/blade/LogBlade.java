c7fe24c64de427276e4a0a8af03f09b6f5d56021
Dominic Langenegger <dola@gmx.ch>
2014-02-03 01:32:21 -0800

    added Blades project

--- workspace/Blades/src/edu/uw/bladedroid/blade/LogBlade.java
+++ workspace/Blades/src/edu/uw/bladedroid/blade/LogBlade.java
@@ -1,14 +1,14 @@
 package edu.uw.bladedroid.blade;
 
-import edu.uw.bladedroid.AbstractBlade;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import edu.uw.bladedroid.AbstractBlade;
 
-public class TestBlade extends AbstractBlade {
-
+public class LogBlade extends AbstractBlade {
+	
 	public void onCreate(Activity activity, Bundle savedInstanceState) {
-		Log.i("BLADE", "THIS IS THE TEST BLADE on activity " + activity.getClass().getName());
+		Log.wtf("BLADE", "THIS IS THE TEST BLADE on activity " + activity.getClass().getName());
 	}
-
+	
 }
---
+++ workspace/Blades/src/edu/uw/bladedroid/blade/ToastBlade.java
@@ -0,0 +1,16 @@
+package edu.uw.bladedroid.blade;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import edu.uw.bladedroid.AbstractBlade;
+
+public class ToastBlade extends AbstractBlade {
+	
+	@Override
+	public void onResume(Activity activity, Bundle savedInstanceState) {
+		Toast.makeText(activity.getApplicationContext(), "Hello World!",
+				   Toast.LENGTH_LONG).show();
+	}
+	
+}
