39dca269a35813e98c1ea8989f2e530b33492f29
Dominic Langenegger <dola@gmx.ch>
2014-02-03 18:56:51 -0800

    remove unused savedInstanceState from IBlade event handlers

--- workspace/DexLoader/src/edu/uw/bladedroid/IBlade.java
+++ workspace/DexLoader/src/edu/uw/bladedroid/IBlade.java
@@ -4,47 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public interface IBlade {
-	/**
-	 * 
-	 * @param activity
-	 * @param savedInstanceState
-	 * @see Activity#onCreate
-	 */
-	void onCreate(Activity activity, Bundle savedInstanceState);
-	/**
-	 * 
-	 * @param activity
-	 * @param savedInstanceState
-	 * @see Activity#onStart
-	 */
-	void onStart(Activity activity, Bundle savedInstanceState);
-	/**
-	 * 
-	 * @param activity
-	 * @param savedInstanceState
-	 * @see Activity#onResume
-	 */
-	void onResume(Activity activity, Bundle savedInstanceState);
-	/**
-	 * 
-	 * @param activity
-	 * @param savedInstanceState
-	 * @see Activity#onPause
-	 */
-	void onPause(Activity activity, Bundle savedInstanceState);
-	/**
-	 * 
-	 * @param activity
-	 * @param savedInstanceState
-	 * @see Activity#onStrop
-	 */
-	void onStop(Activity activity, Bundle savedInstanceState);
-	/**
-	 * 
-	 * @param activity
-	 * @param savedInstanceState
-	 * @see Activity#onDestroy
-	 */
-	void onDestroy(Activity activity, Bundle savedInstanceState);
-	
+    /**
+     * 
+     * @param activity
+     * @param savedInstanceState
+     * @see Activity#onCreate
+     */
+    void onCreate(Activity activity, Bundle savedInstanceState);
+
+    /**
+     * 
+     * @param activity
+     * @see Activity#onStart
+     */
+    void onStart(Activity activity);
+
+    /**
+     * 
+     * @param activity
+     * @see Activity#onResume
+     */
+    void onResume(Activity activity);
+
+    /**
+     * 
+     * @param activity
+     * @see Activity#onPause
+     */
+    void onPause(Activity activity);
+
+    /**
+     * 
+     * @param activity
+     * @see Activity#onStrop
+     */
+    void onStop(Activity activity);
+
+    /**
+     * 
+     * @param activity
+     * @see Activity#onDestroy
+     */
+    void onDestroy(Activity activity);
+
 }
