00c1e8d4116c48deaad1c046407bd25a704955f3
Pingyang He <isphrazy@hotmail.com>
2014-03-13 22:53:42 -0700

    rm bin, add modification

--- workspace/Blades/src/edu/uw/bladedroid/blade/ActionReplay.java
+++ workspace/Blades/src/edu/uw/bladedroid/blade/ActionReplay.java
@@ -30,14 +30,11 @@ public class ActionReplay extends AbstractBlade{
     private final Mode MODE = Mode.REPLAY;
     
     private TouchingEventSaver saver;
-//    private List<MotionEvent> mes;
     
  
     public void onCreate(Activity activity, Bundle savedInstanceState) {
         Log.wtf(LOG_TAG, "in " + MODE + " mode");
         saver = TouchingEventSaver.getInstance(activity);
-//        ViewGroup rootView = (ViewGroup) ((ViewGroup)activity.findViewById(android.R.id.content)).getChildAt(0);
-//        hijectMotionEventApproache(activity);
         if(MODE == Mode.RECORD){
             Toast.makeText(activity, "RECORDING NOW...", Toast.LENGTH_SHORT).show();
             monitorAllButtonsApproache(activity);
@@ -52,13 +49,11 @@ public class ActionReplay extends AbstractBlade{
     private void resetOnClickListenerOfAllButtons(ViewGroup rootView) {
         for(int i = 0; i < rootView.getChildCount(); i++){
             View v = rootView.getChildAt(i);
-            if(v instanceof Button){
-                Button b = (Button)v;
-                final View.OnClickListener listener = getOnClickListener(b);
+                final View.OnClickListener listener = getOnClickListener(v);
                 if(listener != null){
-                    b.setOnClickListener(new ClickMonitListener(listener, b.getId()));
+                    v.setOnClickListener(new ClickMonitListener(listener, v.getId()));
                 }
-            }else if(v instanceof ViewGroup){
+            if(v instanceof ViewGroup){
                 resetOnClickListenerOfAllButtons((ViewGroup)v);
             }
         }
@@ -76,7 +71,6 @@ public class ActionReplay extends AbstractBlade{
 
         @Override
         public void onClick(View v) {
-//            Log.wtf(LOG_TAG, "MONITORINGGGGGGG");
             try {
                 saver.save(viewID, System.currentTimeMillis());
             } catch (IOException e) {
@@ -104,19 +98,15 @@ public class ActionReplay extends AbstractBlade{
                 //give sometime to render UI
                 List<ViewIDTouchTime> idTimes = saver.load();
                 if(idTimes.isEmpty()) return null;
-                Thread.sleep(3000);
+                Thread.sleep(1500);
                 
                 long lastStartTime = 0;
                 for(int i = 0; i < idTimes.size(); i++){
                     ViewIDTouchTime idTime = idTimes.get(i);
-//                        Log.wtf(LOG_TAG, "clicked time: " + idTime.getTime());
                     if(i > 0){
-//                            long nextTime = idTime.getTime();
                         long interval = idTime.getTime() - idTimes.get(i - 1).getTime();
                         long currentTime = System.currentTimeMillis();
-//                            Log.wtf(LOG_TAG, "current time: " + currentTime + " lastStartTime: " + lastStartTime + " interval: " + interval);
                         long idleTime = interval - (currentTime - lastStartTime) ;
-//                            Log.wtf(LOG_TAG, "idleTime: " + idleTime);
                         if(idleTime > 0){
                             try {
                                 Thread.sleep(idleTime);
@@ -151,16 +141,12 @@ public class ActionReplay extends AbstractBlade{
     
     private static class TouchingEventSaver{
         
-//        private final String FILE_NAME = "TOUCH_EVENTS";
         private String fileName;
         
         private static TouchingEventSaver instance;
         private Context context;
-//        private FileOutputStream fos;
         private BufferedWriter writer;
         private List<ViewIDTouchTime> touchingSeq;
-//        private 
-//        private 
         
         private TouchingEventSaver(Context context){
             this.context = context;
@@ -300,134 +286,6 @@ public class ActionReplay extends AbstractBlade{
         return retrievedListener;
     }
 
-//    private void hijectMotionEventApproache(Activity activity){
-//        ViewGroup rootView = (ViewGroup)activity.findViewById(android.R.id.content);
-//        LinearLayout newLayer = new ActionReplayLayout(activity);
-////        newLayer.setOnTouchListener(new OnTouchListener(){
-////
-////            @Override
-////            public boolean onTouch(View arg0, MotionEvent arg1) {
-////                Log.wtf(LOG_TAG, "new layer is touched");
-////                return false;
-////            }
-////            
-////        });
-//        final LayoutParams lp = newLayer.getLayoutParams();
-//        newLayer.setPadding(0, 0, 0, 0);
-//        View overallView = rootView.getChildAt(0);
-//        rootView.removeView(overallView);
-//        rootView.addView(newLayer);
-//        newLayer.addView(overallView);
-////        try {
-////            newLayer = rootView.getClass().getDeclaredConstructor(Context.class).newInstance(activity);
-////            newLayer.setPadding(0, 0, 0, 0);
-////            Log.wtf(LOG_TAG, "new layer has type: " + newLayer.getClass().getName());
-//////            newLayer.
-////        } catch (InstantiationException e) {
-////            // TODO Auto-generated catch block
-////            e.printStackTrace();
-////        } catch (IllegalAccessException e) {
-////            // TODO Auto-generated catch block
-////            e.printStackTrace();
-////        } catch (IllegalArgumentException e) {
-////            // TODO Auto-generated catch block
-////            e.printStackTrace();
-////        } catch (InvocationTargetException e) {
-////            // TODO Auto-generated catch block
-////            e.printStackTrace();
-////        } catch (NoSuchMethodException e) {
-////            // TODO Auto-generated catch block
-////            e.printStackTrace();
-////        }
-////        if(newLayer == null) {
-////            Log.wtf(LOG_TAG, "failed to create new layer");
-////            return;
-////        }
-////
-////        for(int i = 0; i < rootView.getChildCount(); i++){
-////            View v = rootView.getChildAt(i);
-////            Log.wtf(LOG_TAG, "view has type: " + v.getClass().getName());
-////            rootView.removeView(v);
-////            newLayer.addView(v);
-////        }
-//
-////        Set<Integer> viewIDs = getAllViewsID(rootView);
-////        if(MODE == Mode.RECORD){
-////            //add listeners
-////            for(Integer id : viewIDs){
-////                View v = activity.findViewById(id);
-////                
-////            }
-////        }else if(MODE == Mode.REPLAY){
-////            //replay actions
-////        }
-//    }
-//    
-//    @Override
-//    public void onResume(Activity activity) {
-//        Log.wtf(LOG_TAG, "in on resume");
-//        if(!mes.isEmpty()){
-//            Log.wtf(LOG_TAG, "mes not empty: " + mes.size());
-////            activity.onTouchEvent(mes.get(0));
-//            for(MotionEvent me : mes){
-//                Log.wtf(LOG_TAG, "x: " + me.getX() + " y: " + me.getY());
-//                Log.wtf(LOG_TAG, "actions: " + MotionEvent.ACTION_DOWN);
-//                MotionEvent sim = MotionEvent.obtain(
-//                        SystemClock.uptimeMillis(), 
-//                        SystemClock.uptimeMillis(), 
-//                        me.getAction(), 
-//                        me.getX(), 
-//                        me.getY(),
-//                        0);
-//                activity.dispatchTouchEvent(sim);
-//                break;
-//            }
-//            
-////            activity.
-////            for(MotionEvent me : mes){
-////                activity.onTouchEvent(me);
-//////                try {
-//////                    Thread.sleep(1000);
-//////                } catch (InterruptedException e) {
-//////                    // TODO Auto-generated catch block
-//////                    e.printStackTrace();
-//////                }
-////            }
-//        }
-//    }
-//    
-//    private class ActionReplayLayout extends LinearLayout{
-//
-//        public ActionReplayLayout(Context context) {
-//            super(context);
-//        }
-//        
-////        public boolean dispatchTouchEvent(MotionEvent ev){
-//        public boolean onInterceptTouchEvent(MotionEvent ev){
-//            Log.wtf(LOG_TAG, "in onInterceptTouchEvent touch event");
-//            
-//            mes.add(ev);
-//            Log.wtf(LOG_TAG, "record x: " + ev.getX() + " y: " + ev.getY());
-//            Log.wtf(LOG_TAG, "record actions: " + MotionEvent.ACTION_DOWN);
-//            return super.onInterceptTouchEvent(ev);
-//        }
-//        
-//    }
-//
-//    private Set<Integer> getAllViewsID(View inputView) {
-//        ViewGroup viewgroup = (ViewGroup)inputView;
-//        Set<Integer> result = new HashSet<Integer>();
-//        for(int i = 0; i < viewgroup.getChildCount(); i++){
-//            View v = viewgroup.getChildAt(i);
-//            result.add(v.getId());
-//            if(v instanceof ViewGroup){
-////              Log.wtf("ADSBLOCKER", "instance of viewgroup");
-//                result.addAll(getAllViewsID(v));
-//            }
-//        }
-//        return result;
-//    }
-    
     public enum Mode{
         RECORD, REPLAY, NONE
     }
